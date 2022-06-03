b23b56e19b05ba3844709f71da29139f4ba9ad04
tatiana <105023527+tatianajeon@users.noreply.github.com>
2022-06-03 16:13:56 -0500

    final draft

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -8,7 +8,8 @@ def play():
 
     # Step 1: Greet & ask for player name.
     print("\nWelcome to rock paper scissors!\n")
-    name = input("\nwhat is your name?: ")
+    name = input("\nWhat is your name? ")
+    name = name.lower().strip()
     score_board[name] = {}
     y=0
     c=0
@@ -23,34 +24,36 @@ def play():
             break
 
         # Step 2: Choose to play
-        start  = input("\nWould you like to play? (type 'yes' or 'no'): ")
+        start  = input(" \nHow To Play: \n\nType 'rock', 'paper', or 'scissors' into the input. \n\nRock beats Scissors, Scissors beats Paper, and Paper beats Rock. \n\nTo add a new player type 'new'.  \n\nTo switch players type 'switch'.  \n\nTo end the game type 'quit'.    \n\n\n\nWould you like to play? (type 'yes' or 'no'): ")
+        start = start.lower().strip()
 
         # Step 3a: Play Game
-        if start.lower().strip() == 'yes':
+        if start == 'yes':
             while True:
                 
                 
 
                 # Step4: User chooses option
                 user_input = input("\nChoose 'rock', 'paper', or 'scissors': ") 
-                computer_input  = choice(choices)
+                user_input = user_input.lower().strip()
+                computer_input = choice(choices)
 
                 # Step5a: User Chooses Rock
                 if user_input == 'rock' and computer_input == 'paper':
                     print(f"\nThe computer chose {computer_input}, you lose!")
                     c += 1  
                     score_board[name]['computer'] = c
-                    print(f"\n{score_board[name]}")
+                    print(f"{score_board[name]}")
                     
                 elif user_input == 'rock' and computer_input == 'scissors':
                     print(f"\nThe computer chose {computer_input}, you win!")
                     y += 1  
                     score_board[name]['you'] = y
-                    print(f"\n{score_board[name]}")
+                    print(f"{score_board[name]}")
 
                 elif user_input == 'rock' and computer_input == 'rock':
                     print(f"\nThe computer chose {computer_input}, it's a tie!") 
-                    print(f"\n{score_board[name]}")
+                    print(f"{score_board[name]}")
 
 
 
@@ -58,19 +61,19 @@ def play():
                 # Step 5b: User Chooses Paper    
                 elif user_input == 'paper' and computer_input == 'paper':
                     print(f"\nThe computer chose {computer_input}, it's a tie!") 
-                    print(f"\n{score_board[name]}")
+                    print(f"{score_board[name]}")
                     
                 elif user_input == 'paper' and computer_input == 'scissors':
                     print(f"\nThe computer chose {computer_input}, you lose!")
                     c += 1  
                     score_board[name]['computer'] = c
-                    print(f"\n{score_board[name]}")
+                    print(f"{score_board[name]}")
 
                 elif user_input == 'paper' and computer_input == 'rock':
                     print(f"\nThe computer chose {computer_input}, you win!")  
                     y += 1  
                     score_board[name]['you'] = y 
-                    print(f"\n{score_board[name]}")
+                    print(f"{score_board[name]}")
 
 
 
@@ -80,17 +83,17 @@ def play():
                     print(f"\nThe computer chose {computer_input}, you win!")
                     y += 1  
                     score_board[name]['you'] = y 
-                    print(f"\n{score_board[name]}")
+                    print(f"{score_board[name]}")
 
                 elif user_input == 'scissors' and computer_input == 'scissors':
                     print(f"\nThe computer chose {computer_input}, its a tie!")    
-                    print(f"\n{score_board[name]}")
+                    print(f"{score_board[name]}")
                     
                 elif user_input == 'scissors' and computer_input == 'rock':
                     print(f"\nThe computer chose {computer_input}, you lose!")
                     c += 1  
                     score_board[name]['computer'] = c
-                    print(f"\n{score_board[name]}")
+                    print(f"{score_board[name]}")
                 
 
 
@@ -99,17 +102,27 @@ def play():
                 elif user_input == 'quit':
                     break_flag = True
                     print("\nThanks for playing!")
-                    print(f"\n{score_board[name]}")
+                    print(f"\n{score_board}") 
                     break
 
+                elif user_input == 'switch':
+                    name = input("Who would you like to switch to? ")
+                    name = name.lower().strip()
+                    if name in score_board:
+                        print(score_board[name])
+                        y = score_board[name]['you']
+                        c = score_board[name]['computer']
+                        score_board[name] = score_board[name]
+
                 elif user_input == 'new':
-                    y=0
-                    c=0
+                    y= 0
+                    c= 0
                     name = input("\nWhose playing now?: ")
+                    name = name.lower().strip()
                     score_board[name] = {}
 
                 else:
-                    print("\ninvalid input...")
+                    print("\ninvalid input... ")
 
         # Step 5d1: End game
         elif break_flag:
@@ -117,7 +130,7 @@ def play():
             break
         
         # Step 3b: Don't play
-        elif start.lower().strip() == 'no':
+        elif start == 'no':
             print("\nThanks for visiting!")
             break
         
@@ -128,12 +141,4 @@ def play():
 play()
 
 
-
-
-# TO DO LIST
-#
-# extra feature: ie, name
-
- 
-# scoreboard = {}
 
